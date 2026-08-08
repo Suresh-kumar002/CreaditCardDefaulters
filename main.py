3f39a43e032f06297463d2e052e1d6edc6d103d2
Suresh kumar <officialsuru2983@gmail.com>
2026-08-08 18:44:43 +0530

    Prepare Heroku deployment

--- main.py
+++ main.py
@@ -91,6 +91,6 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successfull!!")
 
-port = int(os.getenv("PORT",5001))
+port = int(os.getenv("PORT", 5001))
 if __name__ == "__main__":
-    app.run(port=port,debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
