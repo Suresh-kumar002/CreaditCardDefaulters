724893fd76bc64a1818bf335764f64afd0ad9338
Suresh kumar <officialsuru2983@gmail.com>
2026-08-08 18:39:29 +0530

    Add Vercel deployment support

---
+++ api/index.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
+
+from main import app
+
+if __name__ == '__main__':
+    app.run()
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import os
 from flask import Flask, request, render_template
 from flask import Response
-import os
 from flask_cors import CORS, cross_origin
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(BASE_DIR)
+
 from prediction_Validation_Insertion import pred_validation
 from trainingModel import trainModel
 from training_Validation_Insertion import train_validation
-#import flask_monitoringdashboard as dashboard
+# import flask_monitoringdashboard as dashboard
 from predictFromModel import prediction
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(BASE_DIR, 'templates'))
 #dashboard.bind(app)
 CORS(app)
 
